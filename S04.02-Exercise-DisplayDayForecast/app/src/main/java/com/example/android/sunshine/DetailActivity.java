b359e3a6a79a653a8900fc3b33d208a5d34b0b57
Victor Letunovsky <Victor.Letunovsky@gmail.com>
2019-05-24 00:50:02 +0300

    [S04.02-Exercise-DisplayDayForecast]
    1. Use the putExtra method to add the String that was clicked into the Intent used to start the DetailActivity.
    2. Safely retrieve the extra String from the Intent inside the DetailActivity.
    3. Add a TextView to your layout and set the text of the TextView with the String from the Intent extra.

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        TextView detailTextView = findViewById(R.id.tv_activity_detail);
+
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+        Intent forecastIntent = getIntent();
+        if (forecastIntent != null && forecastIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            String weatherForecast = forecastIntent.getStringExtra(Intent.EXTRA_TEXT);
+            detailTextView.setText(weatherForecast);
+        }
     }
 }
