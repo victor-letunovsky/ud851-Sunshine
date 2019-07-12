3e32f1b83382c96e0943c5ff1b1cd993cbaf49c6
Victor Letunovsky <Victor.Letunovsky@gmail.com>
2019-07-13 00:00:44 +0300

    [S06.02-Exercise-SettingsFragment]
    1. Show a ListPreference for units and an EditTextPreference for location
    2. Have the summaries for the ListPreference and the EditTextPreference change when updated

---
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,93 @@
+package com.example.android.sunshine;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+
+/**
+ * The SettingsFragment serves as the display for all of the user's settings. In Sunshine, the
+ * user will be able to change their preference for units of measurement from metric to imperial,
+ * set their preferred weather location, and indicate whether or not they'd like to see
+ * notifications.
+ *
+ * Please note: If you are using our dummy weather services, the location returned will always be
+ * Mountain View, California.
+ */
+// COMPLETED (4) Create SettingsFragment and extend PreferenceFragmentCompat
+// COMPLETED (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
+public class SettingsFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener {
+
+    // COMPLETED (8) Create a method called setPreferenceSummary that accepts a Preference and an Object
+    // and sets the summary of the preference
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            /* For list preferences, look up the correct display value in
+               the preference's entries' list (since they have separate labels/values). */
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+    }
+
+    // COMPLETED (11) Override onSharedPreferenceChanged to update non CheckBoxPreferences when they are changed
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (preference == null) {
+            return;
+        }
+        if (preference instanceof CheckBoxPreference) {
+            return;
+        }
+        String value = sharedPreferences.getString(preference.getKey(), "");
+        setPreferenceSummary(preference, value);
+    }
+
+    // COMPLETED (5) Override onCreatePreferences and add the preference xml file using addPreferencesFromResource
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+
+        // Add preferences, defined in the XML file in res -> xml -> pref_general.xml
+        addPreferencesFromResource(R.xml.pref_general);
+
+        // COMPLETED (9) Set the preference summary on each preference that isn't a CheckBoxPreference
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = prefScreen.getSharedPreferences();
+        final int count = prefScreen.getPreferenceCount();
+
+        // Go through all of the preferences, and set up their preference summary.
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    // COMPLETED (12) Register SettingsFragment (this) as a SharedPreferenceChangedListener in onStart
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    // COMPLETED (13) Unregister SettingsFragment (this) as a SharedPreferenceChangedListener in onStop
+    @Override
+    public void onStop() {
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onStop();
+    }
+}
